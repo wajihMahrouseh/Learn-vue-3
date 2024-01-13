2c539b5a83b486a85a40b0fa792bc118fe212506
wajihMahrouseh <wajiho.mahrouseh@gmail.com>
2024-01-13 20:53:25 +0300

    Episode 15 (updated)

--- Episodes/Episode 15, Named Slots/js/components/App.js
+++ Episodes/Episode 15, Named Slots/js/components/App.js
@@ -1,9 +1,52 @@
 import Assignments from "./Assignments.js";
+import Panel from "./Panel.js";
 
 export default {
-    components: { Assignments },
+    components: { Assignments, Panel },
 
     template: `
-        <assignments></assignments>    
+        <assignments></assignments>
+
+
+        <panel>
+            <template>
+                this is the default content.
+            </template>
+        </panel>
+
+
+        <panel>
+            <template v-slot=heading>
+                this is the heading content.
+            </template>
+            
+            this is the default content.
+        </panel>
+
+
+        <panel>
+            <template v-slot=heading>
+                this is the heading content.
+            </template>
+            
+            this is the default content.
+
+            <template v-slot=footer>
+                this is the footer content.
+            </template>
+        </panel>
+
+
+        <panel theme="light">
+            <template #heading>
+                this is the heading content.
+            </template>
+            
+            this is the default content.
+
+            <template #footer>
+                this is the footer content.
+            </template>
+        </panel>
     `,
 }
